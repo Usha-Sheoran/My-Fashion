4731af851e9b7dae2e0db4312c55016c16ec8425
Usha-Sheoran <ushasheoran15@gmail.com>
2025-09-11 13:20:07 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
-
 const { Pool } = require("pg");
-require("dotenv").config(); 
+require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
-
